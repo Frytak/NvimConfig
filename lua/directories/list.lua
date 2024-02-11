bdff0dc5ad42824de523508506d0d38a34ea39d4
Frytak <frytak2855@gmail.com>
2024-02-11 18:43:58 +0100

    SQLS config

--- lua/directories/list.lua
+++ lua/directories/list.lua
@@ -1,8 +1,8 @@
 --- This list assumes that all the directories start from the windows user folder
 local M = {
-    { name = "Nvim config", path = "C:\\Users\\fryta\\AppData\\Local\\nvim\\" },
-    { name = "Programming projects", path = "C:\\Users\\fryta\\Pulpit\\desk\\Important\\Programming Projects\\" },
-    { name = "Downloads", path = "C:\\Users\\fryta\\Downloads\\" },
+    { name = "Nvim config", path = "C:\\Users\\Frytak\\AppData\\Local\\nvim\\" },
+    { name = "Programming projects", path = "C:\\Users\\Frytak\\Desktop\\desk\\Important\\Programming Projects\\" },
+    { name = "Downloads", path = "C:\\Users\\Frytak\\Downloads\\" },
 }
 
 return M
--- lua/plugins/p_sqls.lua
+++ lua/plugins/p_sqls.lua
@@ -3,7 +3,7 @@ return {
     name = 'sqls',
     config = function()
         require('lspconfig').sqls.setup({
-            cmd = { 'sqls', '-config', 'C:\\Users\\fryta\\AppData\\Local\\nvim\\lua\\config\\sqls.yml' },
+            cmd = { 'sqls', '-config', 'C:\\Users\\Frytak\\AppData\\Local\\nvim\\lua\\config\\sqls.yml' },
             --settings = {
             --    sqls = {
             --        connections = {
@@ -16,6 +16,9 @@ return {
             --    },
             --},
             on_attach = function(client, bufnr)
+                vim.keymap.set('n', '<Leader><S-s>', ':SqlsExecuteQuery<Enter>')
+                vim.keymap.set('n', '<Leader>s', '<S-v> :\'<,\'>SqlsExecuteQuery<Enter>')
+                vim.keymap.set('v', '<Leader>s', ':SqlsExecuteQuery<Enter>')
                 require('sqls').on_attach(client, bufnr)
             end
         })
